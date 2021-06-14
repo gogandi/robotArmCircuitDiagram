560f6d31c1acd4edd4fbe763ab83a5cb6497fbfa
gogandi <62101859+gogandi@users.noreply.github.com>
2021-06-14 20:54:30 +0300

    code

---
+++ codeRobotArm (servo).ino
@@ -0,0 +1,42 @@
+// C++ code
+//
+#include <Servo.h>
+
+Servo myservo1;  // create servo object to control a servo
+Servo myservo2; 
+Servo myservo3; 
+Servo myservo4; 
+Servo myservo5; 
+
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo1.attach(8);
+  myservo2.attach(9);
+  myservo3.attach(10);
+  myservo4.attach(11);
+  myservo5.attach(12);// attaches the servo to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo1.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo2.write(pos);
+    myservo3.write(pos);
+    myservo4.write(pos);
+    myservo5.write(pos);
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo1.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo2.write(pos);
+    myservo3.write(pos);
+    myservo4.write(pos);
+    myservo5.write(pos);
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  
+}
